d4b46c26380bf1b51b4237e97193d450313c9e93
Maycon <maycon.dsl@hotmail.com>
2026-04-23 16:34:42 -0300

    :bulb: MudDataGrid para Agendamento

--- BarberShop.Web/Pages/Agendamentos/List.razor.cs
+++ BarberShop.Web/Pages/Agendamentos/List.razor.cs
@@ -12,6 +12,7 @@ namespace BarberShop.Web.Pages.Agendamentos
 
         public bool IsBusy { get; set; } = false;
         public List<Agendamento> Agendamentos { get; set; } = [];
+        public string SearchTerm { get; set; } = String.Empty;
 
         #endregion
 
@@ -19,6 +20,9 @@ namespace BarberShop.Web.Pages.Agendamentos
 
         [Inject]
         public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public IDialogService DialogService { get; set; } = null!;
 
         [Inject]
         public IAgendamentoHandler Handler { get; set; } = null!;
@@ -34,14 +38,14 @@ namespace BarberShop.Web.Pages.Agendamentos
             {
                 var request = new GetAllAgendamentoRequest
                 {
-                    PageNumber = 1, 
-                    PageSize = 25  
+                    PageNumber = 1,
+                    PageSize = 25
                 };
                 var result = await Handler.GetAllAsync(request);
                 if (result.IsSuccess)
                     Agendamentos = result.Data ?? new List<Agendamento>();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Snackbar.Add(ex.Message, Severity.Error);
             }
@@ -50,6 +54,58 @@ namespace BarberShop.Web.Pages.Agendamentos
                 IsBusy = false;
             }
         }
+
+        #endregion
+
+        #region Methods
+
+        public async void OnDeleteButtonClickedAsync(long id, string corte)
+        {
+            var result = await DialogService.ShowMessageBox(
+                "ATENÇÃO",
+                $"Ao prosseguir o agendamento {id} será excluído. Esta é uma ação irreversível! Deseja continuar?",
+                yesText: "EXCLUIR",
+                cancelText: "Cancelar");
+
+            if (result is true)
+                await OnDeleteAsync(id);
+
+            StateHasChanged();
+        }
+
+        public async Task OnDeleteAsync(long id) 
+        {
+            try
+            {
+                await Handler.DeleteAsync(id);
+                Agendamentos.RemoveAll(x => x.Id == id);
+                Snackbar.Add("Agendamento excluído", Severity.Success);
+            }
+            catch(Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+        }
+
+        public Func<Agendamento, bool> Filter => agendamento =>
+        {
+            if (string.IsNullOrEmpty(SearchTerm))
+                return true;
+
+            if (agendamento.Id.ToString().Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            if (agendamento.Corte.Titulo.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            if (agendamento.Data.ToString().Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                return true; 
+            
+            if (agendamento.Tempo.ToString().Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            return false;
+        };
 
         #endregion
     }
